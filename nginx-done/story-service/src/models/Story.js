66e122a2f05c18c73be5b568ebe224d5de37f092
IslamBinJunaed <bsse1019@iit.du.ac.bd>
2022-09-27 13:41:06 +0600

    docker compose debugged

--- nginx-done/story-service/src/models/Story.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import timeStamps from "mongoose-timestamp";
-
-const { Schema, model } = mongoose;
-const storySchema = new Schema({});
-
-storySchema.plugin(timeStamps);
-export const Story = model("Story", storySchema);
